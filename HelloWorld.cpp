b7c76ce527df7ebafddb5b3d428ebd7e1570c442
Pursheld <stompee@bk.ru>
2021-03-14 18:38:57 +0300

    first

---
+++ HelloWorld.cpp
@@ -0,0 +1,34 @@
+﻿
+#include <iostream>
+    //Вызывает функцию std::cout
+void print()
+{   //Вызывает оператор << на обьекте типа std::cout
+    std::cout << "Hello Skillbox!\n";
+}
+/*
+* 2
+* 3
+* 4
+*/
+int main()
+{
+    int x = 100;
+
+    3 + 7;
+
+    int y = x + 100;
+    int mult = x * y;
+
+    int b = 0;
+    b = b + 2;
+
+    int test;
+    test = 100500;
+
+    int test2 = 1005001;
+
+    int random = 1002;
+    std::cout << random;
+        ;
+}
+
